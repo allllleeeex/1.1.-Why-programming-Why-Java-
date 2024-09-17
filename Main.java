a0d9f136a4542caa66ffeb34b045ff6676491b03
allllleeeex <agepp27@gmail.com>
2024-09-17 09:18:06 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,9 @@ public class Main
 {
   public static void main(String[] args)
   {
-  System.out.println = "Hello User!";
-  System.out.println = "...";
-  System.out.println = "Hi...?";
-  System.out.print = "HELLO?!?";
+  System.out.println ("Hello User!");
+  System.out.println = ("...");
+  System.out.println = ("Hi...?");
+  System.out.print = ("HELLO?!?");
   }
 }

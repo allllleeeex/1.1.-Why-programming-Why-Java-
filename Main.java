e8dd0286cc11a8a3aec2d53e3f8a44db1067f229
allllleeeex <agepp27@gmail.com>
2024-09-17 09:04:49 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+  public static void main(String[] args)
+  {
+  System.out.println = "Hello User!";
+  System.out.println = "...";
+  System.out.println = "Hi...?";
+  System.out.print = "HELLO?!?";
+  }
+}
